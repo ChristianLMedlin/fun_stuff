7ba87a46ee334270fb2597624e25e98a440c7fed
Christian Medlin <ChristianLMedlin@gmail.com>
2019-06-21 18:16:58 -0400

    Added Game Over text if player loses the game

--- OOP_snake_Pygame/snake.py
+++ OOP_snake_Pygame/snake.py
@@ -30,6 +30,19 @@ class Snake():
         if self.direction == "down":
             self.y += self.speed
 
+# class Lose():
+#     game = Game()
+#     font = pygame.font.Font('pixel_font.ttf', 32)
+#     text = font.render("Game over!", False, red)
+#     text_surface = text.get_rect()
+#     text_surface.center = (game.display_width / 2, game.display_height / 2)
+#     text_display = pygame.display.set_mode((game.display_width, game.display_height))
+#     text_display.blit(text, text_surface)
+#     pygame.display.update()
+
+
+
+
 class Game():
     display_width = 1000
     display_height = 1000
@@ -37,6 +50,7 @@ class Game():
     snake = Snake()
     snake.y = display_height / 2
     snake.x = display_width / 2
+    play_again = False
     running = True
 
     #This runs once when the main function is called, this initializes required PyGame modules, draws the map, and draws the snake at its starting point.  
@@ -54,12 +68,34 @@ class Game():
         pygame.draw.rect(window, (self.snake.color), (self.snake.x, self.snake.y, self.snake.width, self.snake.height))
         pygame.display.update()
 
+    def game_over(self):
+        font = pygame.font.Font('pixel_font.ttf', 64)
+        text = font.render("Game over!", False, (255, 0, 0))
+        text_surface = text.get_rect()
+        text_surface.center = (self.display_width / 2, self.display_height / 2)
+        text_display = pygame.display.set_mode((self.display_width, self.display_height))
+        text_display.blit(text, text_surface)
+        pygame.display.update()
+
+
     #This contains the core functionality of the game, including the main game loop. Pressing Escape or the Exit button will close the PyGame window.
     def main(self):
         self.setup()
         keys = pygame.key.get_pressed()
 
         while self.running:
+            #This causes the player to lose if they go out of bounds.
+
+            #Need to add functionality to restart the game if a certain button is pressed.
+            if self.snake.x > self.display_width or self.snake.x < 0 or self.snake.y > self.display_width or self.snake.y < 0:
+                while not self.play_again:
+                    self.game_over()
+                    for event in pygame.event.get():                
+                        if event.type == pygame.QUIT:
+                            self.running = False
+                            pygame.quit()
+
+
             for event in pygame.event.get():                
                 if event.type == pygame.QUIT:
                     self.running = False
