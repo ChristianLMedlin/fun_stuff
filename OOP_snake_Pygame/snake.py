8ee75914bfafac6e424a978f26bdbf7d6f640ef3
Christian Medlin <ChristianLMedlin@gmail.com>
2019-06-21 22:24:27 -0400

    Player can now play again if SPACE is pushed, cleaned up code

--- OOP_snake_Pygame/snake.py
+++ OOP_snake_Pygame/snake.py
@@ -36,36 +36,37 @@ class Game():
     display_width = 1000
     display_height = 1000
     display_color = (0, 0, 0)
+    window = pygame.display.set_mode((display_width, display_height))
     snake = Snake()
     snake.y = display_height / 2
     snake.x = display_width / 2
-    play_again = False
     running = True
 
-    #This runs once when the main function is called, this initializes required PyGame modules, draws the map, and draws the snake at its starting point.  
+    #When main() is called, this initializes required PyGame modules, draws the map, and draws the snake at its starting point.  
     def setup(self):
         pygame.init()
-        window = pygame.display.set_mode((self.display_width, self.display_height))
         pygame.display.set_caption("Snake!")
-        window.fill(self.display_color)            
-        pygame.draw.rect(window, (self.snake.color), (self.snake.x, self.snake.y, self.snake.width, self.snake.height))
+        self.window.fill(self.display_color)            
+        pygame.draw.rect(self.window, (self.snake.color), (self.snake.x, self.snake.y, self.snake.width, self.snake.height))
         pygame.display.update()
 
     #This updates the screen to display the current location of all objects.
     def refresh(self, window):
-        window.fill(self.display_color)            
-        pygame.draw.rect(window, (self.snake.color), (self.snake.x, self.snake.y, self.snake.width, self.snake.height))
+        self.window.fill(self.display_color)            
+        pygame.draw.rect(self.window, (self.snake.color), (self.snake.x, self.snake.y, self.snake.width, self.snake.height))
         pygame.display.update()
 
+    #This displays the game over text and resets snake direction and coordinates.
     def game_over(self):
-        font = pygame.font.Font('pixel_font.ttf', 64)
-        text = font.render("Game over!", False, (255, 0, 0))
-        text_surface = text.get_rect()
+        game_over_text = pygame.font.Font('pixel_font.ttf', 64).render("Game over!", False, (255, 0, 0))
+        text_surface = game_over_text.get_rect()
         text_surface.center = (self.display_width / 2, self.display_height / 2)
-        text_display = pygame.display.set_mode((self.display_width, self.display_height))
-        text_display.blit(text, text_surface)
-        pygame.display.update()
+        self.snake.y = self.display_height / 2
+        self.snake.x = self.display_width / 2
+        self.snake.direction = None
 
+        self.window.blit(game_over_text, text_surface)
+        pygame.display.update()     
 
     #This contains the core functionality of the game, including the main game loop. Pressing Escape or the Exit button will close the PyGame window.
     def main(self):
@@ -75,21 +76,26 @@ class Game():
         while self.running:
             #This causes the player to lose if they go out of bounds.
 
-            #Need to add functionality to restart the game if a certain button is pressed.
+            #Game now restarts when space button is pressed, need to add text informing the player.
             if self.snake.x > self.display_width or self.snake.x < 0 or self.snake.y > self.display_width or self.snake.y < 0:
                 death_sound = pygame.mixer.music
                 death_sound.load("Death.ogg")
                 death_sound.set_volume(.5)
                 death_sound.play(0)
+                self.game_over()
+                self.play_again = False
 
                 while not self.play_again:
-                    self.game_over()
+                    pygame.time.delay(100)
+
                     for event in pygame.event.get():                
                         if event.type == pygame.QUIT:
-                            self.running = False
                             pygame.quit()
 
+                        if event.type == pygame.KEYDOWN and event.key ==  pygame.K_SPACE:
+                            self.play_again = True
 
+            #The following code closes the game of the X button is clicked and controls movements for the snake when the arrow keys are pressed.
             for event in pygame.event.get():                
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -109,9 +115,11 @@ class Game():
                         self.running = False
             
             self.snake.update()
-            self.refresh(pygame.display.set_mode((self.display_width, self.display_height)))
+            self.refresh(self.window)
             pygame.time.delay(100)
 
         pygame.quit()
-tester = Game()
-tester.main()
+
+
+play_game = Game()
+play_game.main()
