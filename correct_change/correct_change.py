02528d2f7dea3afe74d6e8de8ea419e13b9971f0
Christian <ChristianLMedlin@gmail.com>
2019-08-02 12:56:51 -0400

    Coding at the beach\!

--- correct_change/correct_change.py
+++ correct_change/correct_change.py
@@ -1,7 +1,16 @@
+number_of_fives = 0
+number_of_ones = 0
+number_of_quarters = 0
+number_of_dimes = 0
+numbers_of_nickels = 0
+number_of_pennies = 0
+
 amount_due = float(input("What is your total bill? $"))
 
-number_of_ones, number_of_quarters, number_of_dimes, numbers_of_nickels, number_of_pennies = 0, 0, 0, 0, 0
 
+while amount_due >= 5:
+    number_of_fives += 1
+    amount_due -= 5
 while amount_due >= 1:
     number_of_ones += 1
     amount_due -= 1
@@ -18,5 +27,7 @@ while amount_due >= .01:
     number_of_pennies += 1
     amount_due -= .01
 
+message = f"You were given {number_of_fives} fives, {number_of_ones}, {number_of_quarters} quarters," \
+    f" {number_of_dimes} dimes, {numbers_of_nickels} nickels, and {number_of_pennies} pennies."
 
-print(f'You were given {number_of_quarters} quarters, {number_of_dimes} dimes, {numbers_of_nickels} nickels, and {number_of_pennies} pennies!')
+print(message)
