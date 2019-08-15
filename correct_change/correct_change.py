d893a3a3b7d49d3174d27fde8b348101cac639d9
Christian Medlin <ChristianLMedlin@gmail.com>
2019-08-14 21:41:09 -0400

    Added 10s

--- correct_change/correct_change.py
+++ correct_change/correct_change.py
@@ -1,3 +1,4 @@
+number_of_tens = 0
 number_of_fives = 0
 number_of_ones = 0
 number_of_quarters = 0
@@ -7,7 +8,9 @@ number_of_pennies = 0
 
 amount_due = float(input("What is your total bill? $"))
 
-
+while amount_due >= 10:
+    number_of_tens += 1
+    amount_due -=10
 while amount_due >= 5:
     number_of_fives += 1
     amount_due -= 5
@@ -27,7 +30,7 @@ while amount_due >= .01:
     number_of_pennies += 1
     amount_due -= .01
 
-message = f"You were given {number_of_fives} fives, {number_of_ones}, {number_of_quarters} quarters," \
+message = f"You were given {number_of_tens} tens, {number_of_fives} fives, {number_of_ones}, {number_of_quarters} quarters," \
     f" {number_of_dimes} dimes, {numbers_of_nickels} nickels, and {number_of_pennies} pennies."
 
 print(message)
