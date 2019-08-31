6eb5e1d953c1f82d7a96109c76839492e864562b
Christian Medlin <ChristianLMedlin@gmail.com>
2019-08-30 20:26:44 -0400

    Adding comments to old code for some reason

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,11 +1,12 @@
 import random
-#Hello
 
+
+# Gets the players choice.
 player_choice = input("Rock, paper, or scissors? ").lower()
 while player_choice not in ["rock", "paper", "scissors"]:
-    player_choice = input("You must enter either: rock, paper, or scissors. ")
+    player_choice = input("You must enter either: rock, paper, or scissors. ").lower()
 
-
+# Randomly selects the computer's option.
 computer_choice = random.randint(1,3)
 if computer_choice == 1:
     computer_choice = "rock"
@@ -14,7 +15,7 @@ elif computer_choice == 2:
 else:
     computer_choice = "scissors"
 
-
+# Compares player_choice to computer_choice and prints out a win or loss.
 if computer_choice == player_choice:
     print("You tied!")
 elif computer_choice == "rock" and player_choice == "paper":
